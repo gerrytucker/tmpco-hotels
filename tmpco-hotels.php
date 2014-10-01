04aaee8eb88667a0088f3ca7a267b787759437c2
Gerry Tucker <gerrytucker@gerrytucker.co.uk>
2014-10-01 12:46:56 +0100

    Initial setup

---
+++ tmpco-hotels.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Plugin Name: tMPCo Hotels
+ * Plugin URI:
+ * Description: The Hotel Management WordPress plugin for the Mayfair Printing Co.
+ * Version: 1.0
+ * Author: Gerry Tucker
+ * Author URI: http://gerrytucker.co.uk/
+ * Text-Domain: tmpco-hotels
+ * GitHub Plugin URI:
+ */
+
+if ( ! class_exists( 'tmpcoHotels' ) ) {
+
+	class tmpcoHotels {
+
+		/**
+		 * Initialize the class
+		 */
+		public function __construct() {
+
+		}
+
+
+	}
+
+}
+
+$tmpcoHotels = new tmpcoHotels();
